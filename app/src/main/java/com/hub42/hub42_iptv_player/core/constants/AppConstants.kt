6bfebe7a35baeb43a6bf4a26351640dd0cfef1c7
rgrlinux <rgrlinux@gmail.com>
2026-02-14 19:41:54 -0300

    add constants

---
+++ app/src/main/java/com/hub42/hub42_iptv_player/core/constants/AppConstants.kt
@@ -0,0 +1,19 @@
+package com.hub42.hub42_iptv_player.core.constants
+
+object AppConstants{
+
+    const val DEFAULT_TIMEOUT = 30L
+
+//     - SharedPreferences
+    const val PREF_NAME = "hub42_iptv_prefs"
+    const val KEY_LAST_CHANNEL_URL = "last_channel_url"
+    const val KEY_IS_LOGGED_IN = "is_logged_in"
+    const val KEY_USER_LIST_URL = "user_m3u_url"
+//     Log tags
+    const val TAG_DEBUG = "HUB42_DEBUG"
+    const val TAG_ERROR = "HUB42_ERROR"
+    const val TAG_PLAYER = "HUB42_PLAYER"
+//    Player Agent
+    const val PLAYER_USER_AGENT = "Hub42-IPTV-Player/1.0"
+
+}
